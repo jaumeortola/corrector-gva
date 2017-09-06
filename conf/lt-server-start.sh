1301d797e06379861e4694522de0a89e382c1807
jaumeortola <jaumeortola@gmail.com>
2017-09-06 21:28:52 +0000

    rename folder

--- conf/lt-server-start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-java -Xmx512M -cp ~/languagetool/languagetool-server.jar org.languagetool.server.HTTPServer --config lt-server.properties --port 8081 --public --allow-origin "*" 1>&2 >>lt-server.log &
-
